bc2a2c29ef71f632379c3dbd487c979872c387e7
HowePersonal <howe2004new@gmail.com>
2022-12-20 22:21:17 -0800

    setup basic skeleton code

---
+++ code.js
@@ -0,0 +1,4 @@
+const libraryHeader = document.getElementsByClassName("library-header");
+const libraryInputs = document.getElementsByClassName("library-inputs");
+const libraryContent = document.getElementsByClassName("library-content");
+
