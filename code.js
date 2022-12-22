05b87b6e6e1ca163a4882a5a84876371a2d3071a
HowePersonal <howe2004new@gmail.com>
2022-12-22 15:34:57 -0800

    Added form

--- code.js
+++ code.js
@@ -2,3 +2,22 @@ const libraryHeader = document.getElementsByClassName("library-header");
 const libraryInputs = document.getElementsByClassName("library-inputs");
 const libraryContent = document.getElementsByClassName("library-content");
 
+const bookList = document.getElementsByClassName("content-books");
+
+const form = document.getElementById("form-input")
+const submit = document.getElementById("submit");
+
+function Book(title, author, currentPage) {
+    this.title = title;
+    this.author = author;
+    this.curentPage = currentPage;
+}
+
+submit.addEventListener("submit", addBook());
+
+function addBook(event) {
+    let formData = new FormData(form)
+    event.preventDefault()
+}
+
+const books = [];
