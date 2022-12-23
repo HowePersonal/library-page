7fa59f3e7153985d6d1d79e610f6d135e3db0155
HowePersonal <howe2004new@gmail.com>
2022-12-22 20:40:11 -0800

    Created dynamic webpage with ability to add books with different characteristics and delete books

--- code.js
+++ code.js
@@ -2,10 +2,17 @@ const libraryHeader = document.getElementsByClassName("library-header");
 const libraryInputs = document.getElementsByClassName("library-inputs");
 const libraryContent = document.getElementsByClassName("library-content");
 
-const bookList = document.getElementsByClassName("content-books");
+const titleList = document.getElementById("title-list");
+const authorList = document.getElementById("author-list");
+const pageList = document.getElementById("page-list");
+const deleteList = document.getElementById("delete-list");
 
-const form = document.getElementById("form-input")
+const deleteBook = document.getElementsByClassName("deleteButton");
+
+const form = document.getElementById("form-input");
 const submit = document.getElementById("submit");
+
+const books = [];
 
 function Book(title, author, currentPage) {
     this.title = title;
@@ -13,11 +20,50 @@ function Book(title, author, currentPage) {
     this.curentPage = currentPage;
 }
 
-submit.addEventListener("submit", addBook());
+form.addEventListener("submit", function(event) {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let currentPage = document.getElementById("currentPage").value;
+    event.preventDefault();
+    
+    let book = new Book(title, author, currentPage);
+    books.push(book);   
+    updateList(title, author, currentPage, books.length-1);
+    console.log(books)
+    form.reset();
+    
+});
 
-function addBook(event) {
-    let formData = new FormData(form)
-    event.preventDefault()
+function updateList(title, author, currentPage, bookIndex) {
+    let titleItem = document.createElement("li");
+    let authorItem = document.createElement("li");
+    let pageItem = document.createElement("li");
+    let deleteItem = document.createElement("li");
+    let deleteButton = document.createElement("button");
+
+    titleItem.innerHTML = "<div>" + title + "</div>"
+    authorItem.innerHTML = "<div>" + author + "</div>"
+    pageItem.innerHTML = "<div>" + currentPage + "</div>"
+
+    deleteButton.append("Delete")
+    deleteButton.classList.add("deleteButton")
+    deleteItem.appendChild(deleteButton)
+
+    titleItem.setAttribute("data-index", bookIndex)
+    authorItem.setAttribute("data-index", bookIndex)
+    pageItem.setAttribute("data-index", bookIndex)
+    deleteItem.setAttribute("data-index", bookIndex)
+    deleteButton.setAttribute("data-index", bookIndex)
+
+    titleList.appendChild(titleItem)
+    authorList.appendChild(authorItem)
+    pageList.appendChild(pageItem)
+    deleteList.appendChild(deleteItem)
 }
 
-const books = [];
+deleteList.addEventListener("click", function(e) {
+    let itemIndex = e.target.dataset.index
+    books.splice(itemIndex, 1)
+    document.querySelectorAll("[data-index='"+itemIndex+"']").forEach(e => e.remove())
+
+})
